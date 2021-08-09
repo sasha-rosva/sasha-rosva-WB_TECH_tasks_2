1436e86adf655f0c4c5998bef2661e22552ccd5f
sasha-rosva <63851438+sasha-rosva@users.noreply.github.com>
2021-08-09 22:47:56 +0300

    Create main.go

---
+++ WB_TECH_2_code_5/main.go
@@ -0,0 +1,24 @@
+package main
+
+type customError struct {
+	msg string
+}
+func (e *customError) Error() string {
+	return e.msg
+}
+func test() *customError {
+	{
+		// do something
+	}
+	return nil
+}
+func main() {
+	var err error // err сейчас является нулевым интерфейсом err==nil (true)
+	err = test() // здесь err содержит информацию о типе переменной.
+	// Тип переменной - это  *customError. Соответственно err уже не будет nil.
+	if err != nil {
+		println("error")
+		return
+	}
+	println("ok")
+}
