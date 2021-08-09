ee00be9264c832e377b693ed10943e7d0c6f5a22
sasha-rosva <63851438+sasha-rosva@users.noreply.github.com>
2021-08-09 22:17:45 +0300

    Create main.go

---
+++ WB_TECH_2_паттерн_фасад/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type SmartHouseFacade struct {
+	Water *Water
+	Light *Light
+	Tv    *Tv
+}
+type Water struct {}
+type Light struct {}
+type Tv struct {}
+func NewSmartHouseFacade()*SmartHouseFacade{
+	return &SmartHouseFacade{}
+}
+func (w *SmartHouseFacade) IAmHome(){
+	fmt.Println("Запущен режим: 'Я дома!'")
+	w.Water.TurnOnWater()
+	w.Light.SwitchOnLight()
+	w.Tv.ActivateTv()
+}
+func (w *SmartHouseFacade) IGoAway(){
+	fmt.Println("Запущен режим: 'Я ушел!'")
+	w.Water.TurnOffWater()
+	w.Light.SwitchOffLight()
+	w.Tv.DeactivateTv()
+}
+func (w *Water) TurnOnWater(){
+	fmt.Println("Вода включена!")
+}
+func (w *Water) TurnOffWater(){
+	fmt.Println("Вода выключена!")
+}
+func (w *Light) SwitchOnLight(){
+	fmt.Println("Свет включен!")
+}
+func (w *Light) SwitchOffLight(){
+	fmt.Println("Свет выключен!")
+}
+func (w *Tv) ActivateTv(){
+	fmt.Println("Телевизор включен!")
+}
+func (w *Tv) DeactivateTv(){
+	fmt.Println("Телевизор выключен!")
+}
+func main(){
+	MySmartHouseFacade:=NewSmartHouseFacade()
+	MySmartHouseFacade.IAmHome()
+	time.Sleep(3*time.Second)
+	MySmartHouseFacade.IGoAway()
+}
