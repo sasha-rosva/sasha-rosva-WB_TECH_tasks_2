ff930fb15b3253fee27e469818fd9e1c9883b82c
sasha-rosva <63851438+sasha-rosva@users.noreply.github.com>
2021-08-09 22:27:29 +0300

    Create main.go

---
+++ WB_TECH_2_паттерн_фабричный метод/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Player interface {
+	GetName() string
+	GetAge() int
+	GetGoals() int
+}
+
+func getFootballPlayer(role string) (Player, error) {
+	if role == "goalkeeper" {
+		return newGoalkeeper(), nil
+	}
+	if role == "field player" {
+		return newFieldPlayer(), nil
+	}
+	return nil, errors.New("wrong player type passed")
+}
+func main() {
+	player1, _ := getFootballPlayer("goalkeeper")
+	player2, _ := getFootballPlayer("field player")
+	printDetails(player1)
+	fmt.Println()
+	printDetails(player2)
+
+	fmt.Println()
+	player4, err := getFootballPlayer("coach")
+	if err == nil {
+		printDetails(player4)
+	} else {
+		fmt.Println(err)
+	}
+
+}
+
+func printDetails(p Player) {
+	fmt.Printf("Name: %s\n", p.GetName())
+	fmt.Printf("Age: %d\n", p.GetAge())
+	fmt.Printf("Goals: %d\n", p.GetGoals())
+}
